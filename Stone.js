e35571db710fdfb9aacbe9e5d40a53e8fa6caed6
KishaBhatia <64302865+KishaBhatia@users.noreply.github.com>
2020-05-31 18:21:29 +0530

    Update Stone.js

--- Stone.js
+++ Stone.js
@@ -1,8 +1,8 @@
 class Stone {
     constructor(x, y,width,height) {
       var options = {
-          'restitution':0.3,
-          'friction':1.2,
+          'restitution':1.0,
+          //'friction':1.2,
           //'density':1.5
       }
       this.body = Bodies.rectangle(x, y, width, height, options);
